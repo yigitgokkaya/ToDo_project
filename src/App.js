f79ad129ccfbd78abc14058d236191735edfdb9a
Yiğit Gökkaya <gokkayayigit@gmail.com>
2022-11-13 11:06:51 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ import Navbar from "./pages/Navbar";
 import Login from "./pages/Login";
 import Error from "./pages/Error";
 
-/* todo check login credentials and for users to get to home page*/
+/* todo--- check login credentials and for users to get to home page*/
 function App() {
   return (
     <BrowserRouter>
